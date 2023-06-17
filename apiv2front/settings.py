68471581d7fef2ae328dd8c8dde6d9981dbc5dd7
Salih Can <salihcan9946@gmail.com>
2023-06-17 15:59:03 +0300

    x45

--- apiv2front/settings.py
+++ apiv2front/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uk=#imci-&jsx2x%0w20%lyp26$)eqtf+8py#vsm!i()@23b86'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
