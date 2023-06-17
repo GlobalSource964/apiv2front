11cdc938ebab349507853c202bc4414bb9660d92
Salih Can <salihcan9946@gmail.com>
2023-06-17 16:05:37 +0300

    x46

--- apiv2front/urls.py
+++ apiv2front/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from django.urls import path
 from api_app.views import index
 from apiv2front import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('robots.txt', serve, {'document_root': settings.BASE_DIR, 'path': 'robots.txt'}),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
