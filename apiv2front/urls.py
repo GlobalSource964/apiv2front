2391e006b0f53bb3d87aaf497aeb1acb8ef818d7
Salih Can <salihcan9946@gmail.com>
2023-06-20 05:34:57 +0300

    x74

--- apiv2front/urls.py
+++ apiv2front/urls.py
@@ -8,7 +8,7 @@ from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', redirect_to_amp),
+    path('', redirect_to_amp),
     path('amp', index),
     path('robots.txt', serve, {'document_root': settings.BASE_DIR, 'path': 'robots.txt'}),
     path('sitemap.xml', views.sitemap),
